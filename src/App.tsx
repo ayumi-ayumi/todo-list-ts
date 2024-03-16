bc5add24dbc8dc1e0829a96d96859790e42f2176
Ayumi <ayuayuayuayus@gmail.com>
2024-03-16 13:10:20 +0100

    Styling the input form

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { FormEvent, useState, useEffect } from "react";
+import { FormEvent, useState, useEffect, SetStateAction } from "react";
 import "./App.css";
 import { v4 as uuidv4 } from "uuid";
 import Input from "@mui/joy/Input";
@@ -7,19 +7,16 @@ import DeleteIcon from "@mui/icons-material/Delete";
 import EditIcon from "@mui/icons-material/Edit";
 import CheckCircleOutlineSharpIcon from "@mui/icons-material/CheckCircleOutlineSharp";
 import { pink } from "@mui/material/colors";
-import Box from '@mui/material/Box';
-import InputLabel from '@mui/material/InputLabel';
-import MenuItem from '@mui/material/MenuItem';
-import FormControl from '@mui/material/FormControl';
-import Select from '@mui/material/Select';
+import Select from "@mui/joy/Select";
+import Option from "@mui/joy/Option";
 
 function App() {
   const [inputText, setInputText] = useState("");
-  // const [todos, setTodos] = useState<Todo[]>(()=>loadTodos());
   const [todos, setTodos] = useState<Todo[]>(() =>
     JSON.parse(localStorage.getItem("TODOS") ?? "[]")
   );
   const [addToggle, setAddToggle] = useState<boolean>(false);
+  const [tab, setTab] = useState<string>("all");
 
   type Todo = {
     id: string;
@@ -29,42 +26,13 @@ function App() {
     date: Date;
   };
 
-  // useEffect(() => {
-  //   // const todoJSON = localStorage.getItem('TODOS')
-  //   // console.log(todoJSON)
-  //   // if (todoJSON) {
-  //   //     const todoJSONParse = JSON.parse(todoJSON)
-  //   //     setTodos(todoJSONParse)
-  //   // }
-  //   const todoJSON = localStorage.getItem("TODOS")
-  //   if (todoJSON) {
-  //     const todoJSONParse = JSON.parse(todoJSON)
-  //     console.log(todoJSONParse)
-
-  //     setTodos(todoJSONParse)
-  //   }
-  //   // if (todoJSON !== null) {
-  //   //   const todoJSONParse = JSON.parse(todoJSON)
-  //   //   setTodos(todoJSONParse)
-  //   // }
-  //   // return JSON.parse(localStorageTodoList ?? "[]");
-  //   // const todoJSON = JSON.parse(localStorage.getItem('TODOS') ?? "[]");
-  //   // if (todoJSON) {
-  //   //   setTodos(todoJSON);
-  //   //  }
-  // }, []);
-
   useEffect(() => {
     localStorage.setItem("TODOS", JSON.stringify(todos));
     setAddToggle(false);
   }, [todos]);
 
-  // function handleChange(e: ChangeEvent<HTMLInputElement>) {
-  //   e.preventDefault();
-  //   setInputText(e.target.value);
-  // }
-
-  function handleSubmit(e: FormEvent<HTMLFormElement>) {
+  // Submit a todo task
+  function handleSubmit(e: FormEvent<HTMLFormElement>): void {
     e.preventDefault();
     setAddToggle(true);
 
@@ -79,24 +47,11 @@ function App() {
     };
 
     setTodos([...todos, newTodo]);
-    // setInputText(inputText);
     setInputText("");
-    // saveTodos()
   }
 
-  function handleEditing(id: string) {
-    const todosCopy = todos.map((todo) => ({ ...todo }));
-    setTodos(
-      todosCopy.map((todo) => {
-        if (todo.id === id) {
-          return { ...todo, isEditing: !todo.isEditing };
-        }
-        return todo;
-      })
-    );
-  }
-
-  function handleEditingDone(id: string) {
+  // Toggle an edit boolean
+  function handleEditing(id: string): void {
     const todosCopy = todos.map((todo) => ({ ...todo }));
     setTodos(
       todosCopy.map((todo) => {
@@ -109,7 +64,7 @@ function App() {
   }
 
   //Edit a task
-  function handleEdit(id: string, inputValue: string) {
+  function handleEdit(id: string, inputValue: string): void {
     const todosCopy = todos.map((todo) => ({ ...todo }));
     const newTodos = todosCopy.map((todo) => {
       if (todo.id === id) {
@@ -118,11 +73,10 @@ function App() {
       return todo;
     });
     setTodos(newTodos);
-    // saveTodos();
   }
 
-  //Check a task
-  function handleCompleted(id: string) {
+  // Complete a task
+  function handleCompleted(id: string): void {
     const todosCopy = todos.map((todo) => ({ ...todo }));
     setTodos(
       todosCopy.map((todo) => {
@@ -132,84 +86,87 @@ function App() {
         return todo;
       })
     );
-    // const newTodos = todosCopy.map((todo) => {
-    //   if (todo.id === id) {
-    //     return {...todo, todo.completed: !todo.completed}
-    //   }
-    //   return todo;
-    // });
-    // setTodos(newTodos);
-    // saveTodos();
   }
 
-  // function toggleCompleted(id) {
-  //   setTasks(tasks.map(task => {
-  //   if (task.id === id) {
-  //   return {…task, completed: !task.completed};
-  //   } else {
-  //   return task;
-  //   }
-  //   }));
+  // Choose a filter tab
+  function handleChange (_event: never, newValue: SetStateAction<string>): void {
+    setTab(newValue);
+  }
+
+  // Filter todolist into "Completed" or "Active"
+  function filterTodos(todos: Todo[], tab: string) {
+    return todos.filter((todo) => {
+      if (tab === "all") {
+        return true;
+      } else if (tab === "active") {
+        return !todo.completed;
+      } else if (tab === "completed") {
+        return todo.completed;
+      }
+    });
+  }
+
+  const visibleTodos = filterTodos(todos, tab);
 
   // Delete a task
-  function handleDelete(id: string) {
+  function handleDelete(id: string): void {
     const todosCopy = todos.map((todo) => ({ ...todo }));
     const newTodos = todosCopy.filter((todo) => todo.id !== id);
     setTodos(newTodos);
   }
 
-  // function saveTodos() {
-  //   localStorage.setItem("TODOS", JSON.stringify(todos));
-  // }
-
-  // function loadTodos(): Todo[] {
-  //   const todoJSON = localStorage.getItem("TODOS");
-  //   if (todoJSON == null) return [];
-  //   return JSON.parse(todoJSON);
-  // }
-
   return (
     <div className="App">
-      <div style={{ width: "80%", backgroundColor: "#ffffff" }}>
+      <div
+        style={{
+          width: "80%",
+          backgroundColor: "#ffffff",
+          paddingBottom: "20px",
+        }}
+      >
         <h1>Todo list</h1>
-        <form onSubmit={(e) => handleSubmit(e)}>
-          {addToggle && (
+        <div className="input-container">
+          <form onSubmit={(e) => handleSubmit(e)}>
+            {addToggle && (
+              <Input
+                onChange={(e) => setInputText(e.target.value)}
+                color="neutral"
+                size="lg"
+                variant="outlined"
+                // className="inputText"
+                value={inputText}
+              />
+            )}
             <Input
-              onChange={(e) => setInputText(e.target.value)}
-              color="neutral"
-              size="lg"
-              variant="outlined"
-              // className="inputText"
-              value={inputText}
+              type="submit"
+              value="+ Add Task"
+              color="primary"
+              variant="solid"
+              sx={{ width: "32%" }}
+              className="submitButton"
             />
+          </form>
+          {visibleTodos.length ? (
+            <Select
+              color="neutral"
+              placeholder="All"
+              sx={{ width: "36%" }}
+              variant="solid"
+              onChange={handleChange}
+            >
+              <Option value="all">All</Option>
+              <Option value="completed">Completed</Option>
+              <Option value="active">Active</Option>
+            </Select>
+          ) : (
+            <></>
           )}
-          <Input
-            type="submit"
-            value="+ Add"
-            color="primary"
-            variant="solid"
-            size="lg"
-            className="submitButton"
-          />
-        </form>
-        <FormControl sx={{ m: 1, minWidth: 120 }}>
-        <Select
-          displayEmpty
-          inputProps={{ 'aria-label': 'Without label' }}
-        >
-          <MenuItem value="">
-            <em>None</em>
-          </MenuItem>
-          <MenuItem value={10}>Ten</MenuItem>
-          <MenuItem value={20}>Twenty</MenuItem>
-          <MenuItem value={30}>Thirty</MenuItem>
-        </Select>
-      </FormControl>ï
+        </div>
 
-        {todos.length ? (
+        {visibleTodos.length ? (
           <div className="list-container">
             <ul>
-              {todos.map((todo) => (
+              {visibleTodos.map((todo) => (
                 <li key={todo.id}>
                   <input
                     type="checkbox"
@@ -224,7 +181,9 @@ function App() {
                         size="lg"
                         variant="outlined"
                         value={todo.inputValue}
-                        onChange={(e) => handleEdit(todo.id, e.target.value)}
+                        onChange={(e): void =>
+                          handleEdit(todo.id, e.target.value)
+                        }
                       />
                     ) : (
                       <div>{todo.inputValue}</div>
