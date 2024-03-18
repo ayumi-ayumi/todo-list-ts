36442d5b47ade50be8f182c6fb08afbb6f1663ab
Ayumi <ayuayuayuayus@gmail.com>
2024-03-18 22:19:40 +0100

    vscode has a bug

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -3,16 +3,26 @@ module.exports = {
   env: { browser: true, es2020: true },
   extends: [
     'eslint:recommended',
-    'plugin:@typescript-eslint/recommended',
+    'plugin:@typescript-eslint/recommended-type-checked',
     'plugin:react-hooks/recommended',
+    `plugin:@typescript-eslint/stylistic-type-checked`,
+    `plugin:react/recommended`,
+    `plugin:react/jsx-runtime`,
+    "plugin:react-hooks/recommended"
   ],
-  ignorePatterns: ['dist', '.eslintrc.cjs'],
+  ignorePatterns: ['dist', '.eslintrc.cjs', "vite.config.js"],
   parser: '@typescript-eslint/parser',
-  plugins: ['react-refresh'],
+  plugins: ['react-refresh','react', '@typescript-eslint'],
   rules: {
     'react-refresh/only-export-components': [
       'warn',
       { allowConstantExport: true },
     ],
+    'react/react-in-jsx-scope': 0,
   },
+  settings: {
+    react: {
+      version: "detect",
+    }
+  }
 }
