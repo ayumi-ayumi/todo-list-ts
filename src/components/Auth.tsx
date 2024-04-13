0505eff617b311c69323cfb30034c676549f9185
Ayumi <ayuayuayuayus@gmail.com>
2024-04-13 12:43:28 +0200

    Arranging the style on the Auth component

--- src/components/Auth.tsx
+++ src/components/Auth.tsx
@@ -3,7 +3,7 @@ import { auth } from "../firebase/BaseConfig";
 import {
   createUserWithEmailAndPassword,
   signInWithEmailAndPassword,
-    updateProfile, 
+  updateProfile,
 } from "firebase/auth";
 import { useNavigate } from "react-router-dom";
 // import Button from "@mui/joy/Button";
@@ -46,8 +46,8 @@ export default function Auth() {
       .then((userCredential) => {
         console.log(userCredential.user);
         updateProfile(userCredential.user, {
-        displayName: "Ayumi",
-      })
+          displayName: "Ayumi",
+        })
         navigate("/todolist"); // 登録成功後のリダイレクトページを設定してください。
       })
       .catch((error) => {
@@ -125,72 +125,80 @@ export default function Auth() {
   //     </div>
   //   </>
   // );
+
   return (
     <>
-<Grid item xs={12} sm={8} md={5} component={Paper} elevation={6} square>
-          <Box
-            sx={{
-              my: 8,
-              mx: 4,
-              display: 'flex',
-              flexDirection: 'column',
-              alignItems: 'center',
-            }}
-          >
-            <Avatar sx={{ m: 1, bgcolor: 'secondary.main' }}>
-              <LockOutlinedIcon />
-            </Avatar>
-            <Typography component="h1" variant="h5">
-              Sign in
-            </Typography>
-            <Box component="form" noValidate onSubmit={isLogin ? signUp : logIn} sx={{ mt: 1 }}>
-              <TextField
-                margin="normal"
-                required
-                fullWidth
-                id="email"
-                label="Email Address"
-                name="email"
-                autoComplete="email"
-                autoFocus
-              />
-              <TextField
-                margin="normal"
-                required
-                fullWidth
-                name="password"
-                label="Password"
-                type="password"
-                id="password"
-                autoComplete="current-password"
-              />
-              <FormControlLabel
-                control={<Checkbox value="remember" color="primary" />}
-                label="Remember me"
-              />
-              <Button
-                type="submit"
-                fullWidth
-                variant="contained"
-                sx={{ mt: 3, mb: 2 }}
-              >
-                Sign In
-              </Button>
-              <Grid container>
-                <Grid item xs>
-                  <Link href="#" variant="body2">
-                    Forgot password?
-                  </Link>
-                </Grid>
-                <Grid item>
-                  <Link href="#" variant="body2">
-                    {"Don't have an account? Sign Up"}
-                  </Link>
-                </Grid>
+      {/* <Grid item container direction="column" justifyContent="flex-start" alignItems="center" sx={{ height: '100vh' }}  xs={12} sm={8} md={5} component={Paper} elevation={6} square> */}
+      {/* <Box sx={{ height: '100vh', margin: "0 auto" }} component={Paper} elevation={6} width={500} > */}
+        <Box
+          sx={{
+            px: 10,
+            display: 'flex',
+            flexDirection: 'column',
+            alignItems: 'center',
+            height: '70%', margin: "0 auto", 
+          }}
+          component={Paper} elevation={6} width={400}
+        >
+          <Avatar sx={{ m: 1, bgcolor: 'secondary.main' }}>
+            <LockOutlinedIcon />
+          </Avatar>
+          <Typography component="h1" variant="h5">
+            Sign in
+          </Typography>
+          <Box component="form" noValidate onSubmit={isLogin ? signUp : logIn} sx={{
+            mt: 1, display: 'flex',
+            flexDirection: 'column',
+            alignItems: 'center',
+          }} >
+            <TextField
+              margin="normal"
+              required
+              fullWidth
+              id="email"
+              label="Email Address"
+              name="email"
+              autoComplete="email"
+              autoFocus
+            />
+            <TextField
+              margin="normal"
+              required
+              fullWidth
+              name="password"
+              label="Password"
+              type="password"
+              id="password"
+              autoComplete="current-password"
+            />
+            <FormControlLabel
+              control={<Checkbox value="remember" color="primary" />}
+              label="Remember me"
+            />
+            <Button
+              type="submit"
+              fullWidth
+              variant="contained"
+              sx={{ mt: 3, mb: 2 }}
+            >
+              Sign In
+            </Button>
+            <Grid container>
+              <Grid item xs>
+                <Link href="#" variant="body2">
+                  Forgot password?
+                </Link>
               </Grid>
-            </Box>
+              <Grid item>
+                <Link href="#" variant="body2">
+                  {"Don't have an account? Sign Up"}
+                </Link>
+              </Grid>
+            </Grid>
           </Box>
-        </Grid>
+        </Box>
+      {/* </Box> */}
+      {/* </Grid> */}
     </>
   )
 }
